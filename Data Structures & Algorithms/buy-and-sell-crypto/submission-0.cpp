8f18ab479004ccca201ac8b00ea01a6e51f66a9b
Daniela Tobón <102538771+tobdany@users.noreply.github.com>
2026-06-04 13:00:56 -0600

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        if(prices.empty()) return 0;
+
+        int minPrice=prices[0];
+        int maxProfit=0;
+
+        for(int i=0;i<prices.size();i++){
+            if(prices[i] < minPrice ){
+                minPrice = prices[i];
+            }else if( prices[i] - minPrice > maxProfit ){
+                maxProfit = prices[i] - minPrice;
+            }
+        }
+        return maxProfit;
+        
+    }
+};
